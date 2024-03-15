18770b56e1280dcafdfc0d7f22e964bbb05c0953
Andreas Lilleby Hjulstad <andrelhj@stud.ntnu.no>
2024-03-15 13:43:45 +0100

    change position- and velocity- components to have vectors

--- core/src/main/kotlin/com/peasantrebellion/model/components/PositionComponent.kt
+++ core/src/main/kotlin/com/peasantrebellion/model/components/PositionComponent.kt
@@ -1,8 +1,8 @@
 package com.peasantrebellion.model.components
 
 import com.badlogic.ashley.core.Component
+import com.badlogic.gdx.math.Vector2
 
 class PositionComponent : Component {
-    var x = 0
-    var y = 0
+    var position = Vector2(0f, 0f)
 }
--- core/src/main/kotlin/com/peasantrebellion/model/components/VelocityComponent.kt
+++ core/src/main/kotlin/com/peasantrebellion/model/components/VelocityComponent.kt
@@ -1,8 +1,8 @@
 package com.peasantrebellion.model.components
 
 import com.badlogic.ashley.core.Component
+import com.badlogic.gdx.math.Vector2
 
 class VelocityComponent : Component {
-    var x = 0
-    var y = 0
+    var velocity = Vector2(0f, 0f)
 }
