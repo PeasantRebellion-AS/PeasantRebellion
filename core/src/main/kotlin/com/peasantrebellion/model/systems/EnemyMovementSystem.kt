ea92bfdc9bb801cc50c66b9c4e0bb5cf79ecb608
Andreas Lilleby Hjulstad <andrelhj@stud.ntnu.no>
2024-03-28 15:30:47 +0100

    fix behavior
    It now uses processEntity correctly and iterates only once to move all enemies down

--- core/src/main/kotlin/com/peasantrebellion/model/systems/EnemyMovementSystem.kt
+++ core/src/main/kotlin/com/peasantrebellion/model/systems/EnemyMovementSystem.kt
@@ -10,14 +10,14 @@ import com.peasantrebellion.model.components.BodyComponent
 import com.peasantrebellion.model.components.TextureComponent
 import com.peasantrebellion.model.components.UserControlledComponent
 
-const val ENEMY_MOVEMENT_SPEED = 10f // Temporary value, can be adjusted
+const val ENEMY_MOVEMENT_SPEED = 50f // Temporary value, can be adjusted
 
 class EnemyMovementSystem : IteratingSystem(
     Family.all(
         BodyComponent::class.java,
         TextureComponent::class.java,
         AnimationComponent::class.java,
-    ).get(),
+    ).exclude(UserControlledComponent::class.java).get(),
 ) {
     private val bodyMapper = ComponentMapper.getFor(BodyComponent::class.java)
     private val enemyFamily = Family.all(BodyComponent::class.java).exclude(UserControlledComponent::class.java).get()
@@ -27,26 +27,16 @@ class EnemyMovementSystem : IteratingSystem(
         entity: Entity?,
         deltaTime: Float,
     ) {
-        val enemies = engine.getEntitiesFor(enemyFamily)
-        var moveDown = false
+        val body = bodyMapper[entity].body
+        body.x += ENEMY_MOVEMENT_SPEED * deltaTime * direction
 
-        for (enemy in enemies) {
-            val body = bodyMapper[enemy].body
-
-            body.x += ENEMY_MOVEMENT_SPEED * deltaTime * direction
-
-            if (((body.x + body.width) > Game.WIDTH && direction > 0) || (body.x < 0f && direction < 0)) {
-                // A peasant hit either the right or left side, all enemies should move down and change direction
-                moveDown = true
-                direction = -direction
-            }
-        }
-
-        if (moveDown) {
-            for (e in enemies) {
+        if (((body.x + body.width) > Game.WIDTH && direction > 0) || (body.x < 0f && direction < 0)) {
+            // A peasant hit either the right or left side, all enemies should move down and change direction
+            engine.getEntitiesFor(enemyFamily).forEach { e ->
                 val b = bodyMapper[e].body
                 b.y -= b.height / 2
             }
+            direction = -direction
         }
     }
 }
