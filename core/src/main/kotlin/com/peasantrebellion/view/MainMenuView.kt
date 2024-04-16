f63957bb626dfe234a6db8b3bd0255ef21191de8
runesee <runesee@stud.ntnu.no>
2024-04-16 23:30:54 +0200

    add goofy splash text

--- core/src/main/kotlin/com/peasantrebellion/view/MainMenuView.kt
+++ core/src/main/kotlin/com/peasantrebellion/view/MainMenuView.kt
@@ -5,6 +5,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.peasantrebellion.PeasantRebellion
 import com.peasantrebellion.SCREEN_HEIGHT
 import com.peasantrebellion.SCREEN_WIDTH
+import com.peasantrebellion.model.Game
 import com.peasantrebellion.view.utility.Button
 import com.peasantrebellion.view.utility.MenuFont
 import ktx.app.clearScreen
@@ -20,14 +21,16 @@ class MainMenuView : View {
     private val heightOffset = largeButton.height / 2 + 15f
     private val widthOffset = largeButton.width / 2
     private val crown = Texture("menu/crown.png")
+    private val splashText = Texture("menu/splash_text.png")
+    private val sword = Texture("menu/sword.png")
     val buttons =
         listOf(
             // Singleplayer, multiplayer, leaderboard, tutorial and settings
-            Button(largeButton, WIDTH / 2 - widthOffset, HEIGHT / 2 + 250f),
-            Button(largeButton, WIDTH / 2 - widthOffset, HEIGHT / 2 + 150f),
             Button(largeButton, WIDTH / 2 - widthOffset, HEIGHT / 2 + 50f),
             Button(largeButton, WIDTH / 2 - widthOffset, HEIGHT / 2 - 50f),
             Button(largeButton, WIDTH / 2 - widthOffset, HEIGHT / 2 - 150f),
+            Button(largeButton, WIDTH / 2 - widthOffset, HEIGHT / 2 - 250f),
+            Button(largeButton, WIDTH / 2 - widthOffset, HEIGHT / 2 - 350f),
         )
 
     override fun render() {
@@ -36,7 +39,9 @@ class MainMenuView : View {
         background.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear)
         batch.use {
             it.draw(background, 0f, 0f)
-            it.draw(crown, WIDTH / 2 + 111f, HEIGHT / 2 + largeButton.height / 2 + 206f)
+            it.draw(sword, Game.WIDTH / 2 - sword.width / 2, Game.HEIGHT - sword.height - 95f)
+            it.draw(splashText, Game.WIDTH / 2 - splashText.width / 2, Game.HEIGHT - 400f)
+            it.draw(crown, WIDTH / 2 + 111f, HEIGHT / 2 + largeButton.height / 2 + 6f)
             for (button in buttons) {
                 it.draw(button.texture, button.x, button.y)
             }
@@ -47,31 +52,31 @@ class MainMenuView : View {
             batch,
             "Singleplayer",
             WIDTH / 2 - widthOffset + 18f,
-            HEIGHT / 2 + heightOffset + 250f,
+            HEIGHT / 2 + heightOffset + 50f,
         )
         font.draw(
             batch,
             "Multiplayer",
             WIDTH / 2 - widthOffset + 30f,
-            HEIGHT / 2 + heightOffset + 150f,
+            HEIGHT / 2 + heightOffset - 50f,
         )
         font.draw(
             batch,
             "Leaderboard",
             WIDTH / 2 - widthOffset + 18f,
-            HEIGHT / 2 + heightOffset + 50,
+            HEIGHT / 2 + heightOffset - 150,
         )
         font.draw(
             batch,
             "Tutorial",
             WIDTH / 2 - widthOffset + 75f,
-            HEIGHT / 2 + heightOffset - 50f,
+            HEIGHT / 2 + heightOffset - 250f,
         )
         font.draw(
             batch,
             "Settings",
             WIDTH / 2 - widthOffset + 70f,
-            HEIGHT / 2 + heightOffset - 150f,
+            HEIGHT / 2 + heightOffset - 350f,
         )
         batch.end()
         viewport.camera.update()
